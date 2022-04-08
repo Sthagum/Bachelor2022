8a3858e891be2f246d9b66ce5fcec4fb2d024e3a
stig <59829407+Sthagum@users.noreply.github.com>
2022-04-08 16:29:20 +0200

    Migration uten companylocation

--- QuadBook/Data/Migrations/20220408142337_mangler bare companylocation.Designer.cs
+++ QuadBook/Data/Migrations/20220408142337_mangler bare companylocation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuadBook.Data;
 
@@ -11,9 +12,10 @@ using QuadBook.Data;
 namespace QuadBook.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220408142337_mangler bare companylocation")]
+    partial class manglerbarecompanylocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
