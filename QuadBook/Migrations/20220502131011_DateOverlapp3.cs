cf413d61811c3be78aeb01bbb3169cb6b9635f83
stig <59829407+Sthagum@users.noreply.github.com>
2022-05-02 15:29:22 +0200

    Date overlapping fiks

---
+++ QuadBook/Migrations/20220502131011_DateOverlapp3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuadBook.Migrations
+{
+    public partial class DateOverlapp3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
